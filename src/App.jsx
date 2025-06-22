a21457355868b8478a0c56a43c5c57afa8d56580
evilstein-debug <tejaspathak2005@gmail.com>
2025-06-22 13:06:10 +0530

    update pacman smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ gsap.registerPlugin(ScrollTrigger);
 function App(){
 
     const day1PacmanRef = useRef();
-    const day2PacmanRef = useRef();
+    // const day2PacmanRef = useRef();
     const day1SectionRef = useRef();
-    const day2SectionRef = useRef();
+    // const day2SectionRef = useRef();
+    const wholeSectionRef = useRef(); // New ref for the entire timeline
     
     const day1Events = [
         { event: 'Check-In', time: '8:00 AM' },
@@ -40,30 +41,27 @@ function App(){
             once: false
         });
 
-        // Set initial states
+       // Set initial states
         gsap.set(day1PacmanRef.current, { y: -50, opacity: 0 });
-        gsap.set(day2PacmanRef.current, { y: -50, opacity: 0 });
 
-        // Day 1 Pacman Animation - Simple and Clean
+        // Single continuous Pacman animation for the entire timeline
         gsap.timeline({
             scrollTrigger: {
-                trigger: day1SectionRef.current,
-                start: "top 40%",
-                end: "bottom 10%",
+                trigger: wholeSectionRef.current, // Use the whole section as trigger
+                start: "top 10%",
+                end: "bottom 99%",
                 scrub: 1,
                 // markers: true,
                 onEnter: () => {
-                    console.log("day 1 enter");
+                    console.log("timeline enter");
                     gsap.set(day1PacmanRef.current, { opacity: 1 });
-                    gsap.set(day2PacmanRef.current, { opacity: 0 });
                 },
                 onLeave: () => {
-                    console.log("day 1 leave");
+                    console.log("timeline leave");
                     gsap.set(day1PacmanRef.current, { opacity: 0 });
                 },
                 onEnterBack: () => {
                     gsap.set(day1PacmanRef.current, { opacity: 1 });
-                    gsap.set(day2PacmanRef.current, { opacity: 0 });
                 },
                 onLeaveBack: () => {
                     gsap.set(day1PacmanRef.current, { opacity: 0 });
@@ -73,42 +71,7 @@ function App(){
         .fromTo(day1PacmanRef.current, 
             { y: -50 },
             { 
-                y: day1Events.length * 150 + 50,
-                ease: "none",
-                duration: 1
-            }
-        );
-
-        // Day 2 Pacman Animation - Simple and Clean
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: day2SectionRef.current,
-                start: "top 70%",
-                end: "bottom 85%",
-                scrub: 1,
-                // markers: true,
-                onEnter: () => {
-                    console.log("day 2 enter");
-                    gsap.set(day2PacmanRef.current, { opacity: 1 });
-                    gsap.set(day1PacmanRef.current, { opacity: 0 });
-                },
-                onLeave: () => {
-                    console.log("day 2 leave");
-                    gsap.set(day2PacmanRef.current, { opacity: 0 });
-                },
-                onEnterBack: () => {
-                    gsap.set(day2PacmanRef.current, { opacity: 1 });
-                    gsap.set(day1PacmanRef.current, { opacity: 0 });
-                },
-                onLeaveBack: () => {
-                    gsap.set(day2PacmanRef.current, { opacity: 0 });
-                }
-            }
-        })
-        .fromTo(day2PacmanRef.current, 
-            { y: -50 },
-            { 
-                y: day2Events.length * 155 + 50,
+                y: (day1Events.length * 250),
                 ease: "none",
                 duration: 1
             }
@@ -125,19 +88,19 @@ function App(){
     return (
         <>
         <div className='bg-[#17173B] h-full w-full'>
-            <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col justify-center items-center' ref={wholeSectionRef}>
                 <h1 className='text-5xl text-center text-[#08968A] py-5'
                 style={{fontFamily: 'Pixelify Sans'}}
                 >Schedule</h1>
                 
                 {/* DAY 1 SECTION */}
-                <h2 className='text-3xl text-center text-[#08968A] py-5'
+                <h2 className='text-3xl sm:text-left w-screen max-w-3xl text-[#08968A] py-5'
                 style={{fontFamily: 'Pixelify Sans'}}
                 >DAY - 1</h2>
                 <div className="relative mb-20" ref={day1SectionRef}>
                     {/* Day 1 Vertical Line */}
                     <div className="w-1 bg-[#DC53E6] absolute left-1/2 transform -translate-x-1/2 z-10" 
-                         style={{ height: `${day1Events.length * 150}px`, top: '0px' }}>
+                         style={{ height: `${day1Events.length * 255}px`, top: '0px' }}>
                     </div>
 
                     {/* GSAP Controlled Pacman for Day 1 */}
@@ -158,28 +121,28 @@ function App(){
                 </div>
 
                 {/* DAY 2 SECTION */}
-                <h2 className='text-3xl text-center text-[#08968A] py-5'
+                <h2 className='text-3xl w-screen max-w-3xl sm:text-right text-[#08968A] py-5'
                 style={{fontFamily: 'Pixelify Sans'}}
                 >DAY - 2</h2>
 
-                <div className="relative" ref={day2SectionRef}>
+                <div className="relative">
                     {/* Day 2 Vertical Line */}
-                    <div className="w-1 bg-[#DC53E6] absolute left-1/2 transform -translate-x-1/2 z-10" 
+                    {/* <div className="w-1 bg-[#DC53E6] absolute left-1/2 transform -translate-x-1/2 z-10" 
                          style={{ height: `${day2Events.length * 155}px`, top: '0px' }}>
-                    </div>
+                    </div> */}
 
                     {/* GSAP Controlled Pacman for Day 2 */}
-                    <div 
+                    {/* <div 
                         ref={day2PacmanRef}
                         className='pacman-scroll'
-                    ></div>
+                    ></div> */}
 
                     {day2Events.map((item, index) => (
                         <RoadmapCard 
                             key={`day2-${index}`}
                             event={item.event}
                             time={item.time}
-                            pos={index}
+                            pos={index+1}
                             animationDelay={index*100}
                         />
                     ))}
